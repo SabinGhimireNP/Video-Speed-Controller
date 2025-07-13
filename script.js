60b333202c5f1c6f91b062a21404dfe4a76c9289
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-13 21:06:50 +0545

    click and drag changes

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ const speed = document.querySelector(".speed");
 const bar = document.querySelector(".speed-bar");
 const video = document.querySelector(".flex");
 
-speed.addEventListener("mousemove", function (e) {
+let pressed = false;
+
+function playbackRate(e) {
+  if (!pressed) return;
+
   const y = e.pageY - this.offsetTop;
   const percent = y / this.offsetHeight;
   const min = 0.4;
   const max = 4;
-  const height = Math.round(percent * 100) + "%";
-  bar.style.height = height;
   const playbackspeed = percent * (max - min) + min;
+  const height = Math.round(percent * 100) + "%";
+
+  bar.style.height = height;
   bar.textContent = playbackspeed.toFixed(1) + "x";
   video.playbackRate = playbackspeed;
-//   console.log([video]);
-});
+}
+speed.addEventListener("mousemove", playbackRate);
+
+speed.addEventListener("mousedown", () => (pressed = true));
+
+speed.addEventListener("mouseup", () => (pressed = false));
+
+speed.addEventListener("mouseleave", () => (pressed = false));
